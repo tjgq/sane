e6202ac5220d4c4cf725693c6fea77e015376e5b
Tiago Quelhas <tiagoq@gmail.com>
2014-11-29 17:06:04 +0000

    Clarify the types of Min, Max and Quant in ConstrRange

--- sane.go
+++ sane.go
@@ -71,6 +71,7 @@ type Info struct {
 
 // A Range is a set of discrete integer or fixed-point values. Value x is in
 // the range if there is an integer k >= 0 such that Min <= k*Quant <= Max.
+// The type of Min, Max and Quant is either int or float64 for all three.
 type Range struct {
 	Min   interface{} // minimum value
 	Max   interface{} // maximum value
